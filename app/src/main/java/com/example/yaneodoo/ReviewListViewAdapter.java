111b3ef3b4ba017af32ac484a8965868a073766b
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2020-07-16 22:13:27 +0900

    first upload

---
+++ app/src/main/java/com/example/yaneodoo/ReviewListViewAdapter.java
@@ -0,0 +1,87 @@
+package com.example.yaneodoo;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class ReviewListViewAdapter extends BaseAdapter {
+    // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
+    private ArrayList<ReviewListViewItem> listViewItemList = new ArrayList<>() ;
+
+    // ListViewAdapter의 생성자
+    public ReviewListViewAdapter() {
+
+    }
+
+    // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
+    @Override
+    public int getCount() {
+        return listViewItemList.size() ;
+    }
+
+    // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴. : 필수 구현
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        final Context context = parent.getContext();
+
+        // "listview_item" Layout을 inflate하여 convertView 참조 획득.
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.review_listview_item, parent, false);
+        }
+
+        // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
+        ImageView iconImageView = (ImageView) convertView.findViewById(R.id.review_imgView) ;
+        TextView dateTextView = (TextView) convertView.findViewById(R.id.review_date_txtView);
+        TextView writerTextView = (TextView) convertView.findViewById(R.id.review_writer_txtView);
+        TextView menuTextView = (TextView) convertView.findViewById(R.id.review_menu_txtView);
+        TextView scoreTextView=(TextView) convertView.findViewById(R.id.review_score_txtView);
+        TextView reviewTextView=(TextView) convertView.findViewById(R.id.review_text_txtView);
+
+        // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
+        ReviewListViewItem listViewItem = listViewItemList.get(position);
+
+        // 아이템 내 각 위젯에 데이터 반영
+        iconImageView.setImageDrawable(listViewItem.getIconDrawable());
+        dateTextView.setText(listViewItem.getDateStr());
+        writerTextView.setText(listViewItem.getWriterStr());
+        scoreTextView.setText(listViewItem.getScoreStr());
+        menuTextView.setText(listViewItem.getMenuStr());
+        reviewTextView.setText(listViewItem.getReviewStr());
+
+        return convertView;
+    }
+
+    // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
+    @Override
+    public long getItemId(int position) {
+        return position ;
+    }
+
+    // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
+    @Override
+    public Object getItem(int position) {
+        return listViewItemList.get(position) ;
+    }
+
+    // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
+    public void addItem(Drawable photo, String date, String writer, String menu, String score, String review) {
+        ReviewListViewItem item = new ReviewListViewItem();
+
+        item.setIconDrawable(photo);
+        item.setDateStr(date);
+        item.setWriterStr(writer);
+        item.setMenuStr(menu);
+        item.setScoreStr(score);
+        item.setReviewStr(review);
+
+        listViewItemList.add(item);
+    }
+}
