14f7f9837edd198b6991f1bc8f8068d729a1a2b2
JinhaJjing <horseesroh@naver.com>
2020-08-10 13:46:18 +0900

    Feat: Add code to check and request permission

--- app/src/main/java/com/example/yaneodoo/SelectUser.java
+++ app/src/main/java/com/example/yaneodoo/SelectUser.java
@@ -1,12 +1,20 @@
 package com.example.yaneodoo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.yaneodoo.Customer.ShowCustomerBistroList;
 import com.example.yaneodoo.Owner.ShowOwnerBistroList;
@@ -14,11 +22,13 @@ import com.example.yaneodoo.Owner.ShowOwnerBistroList;
 public class SelectUser extends AppCompatActivity {
     // FOR ACTIVITY SWITCH. ACCORDING TO USER.
     private Intent intent;
+    static final int SMS_RECEIVE_PERMISSON = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_select);
+        checkSelfPermission();
 
         // 손님 버튼 클릭 리스너
         Button btnCustomer = (Button) findViewById(R.id.customerbtn);
@@ -40,4 +50,44 @@ public class SelectUser extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED)
+                    Log.d("SelectUser", "권한 허용 : " + permissions[i]);
+            }
+        }
+    }
+
+    public void checkSelfPermission() {
+        String temp = "";
+
+        //카메라 권한 확인
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.CAMERA + " ";
+        }
+
+        //파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+
+        //파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+
+        //권한 요청
+        if (TextUtils.isEmpty(temp) == false)
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "), 1);
+        else
+            Toast.makeText(this, "권한을 모두 허용", Toast.LENGTH_SHORT).show();
+    }
+
 }
