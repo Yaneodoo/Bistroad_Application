9c50a8f865080f126c21401de6791ad40823a610
JinhaJjing <horseesroh@naver.com>
2020-08-20 14:08:07 +0900

    fix: Add code to checkbox work properly

--- app/src/main/java/com/example/yaneodoo/ListView/BistroListViewAdapter.java
+++ app/src/main/java/com/example/yaneodoo/ListView/BistroListViewAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -39,10 +40,12 @@ public class BistroListViewAdapter extends BaseAdapter {
         }
 
         // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
-        ImageView iconImageView = (ImageView) convertView.findViewById(R.id.bistro_imgView) ;
-        TextView titleTextView = (TextView) convertView.findViewById(R.id.bistro_name_txtView) ;
-        TextView locationTextView=(TextView) convertView.findViewById(R.id.bistro_location_txtView );
-        TextView descTextView = (TextView) convertView.findViewById(R.id.bistro_desc_txtView) ;
+        ImageView iconImageView = (ImageView) convertView.findViewById(R.id.bistro_imgView);
+        TextView titleTextView = (TextView) convertView.findViewById(R.id.bistro_name_txtView);
+        TextView locationTextView = (TextView) convertView.findViewById(R.id.bistro_location_txtView);
+        TextView descTextView = (TextView) convertView.findViewById(R.id.bistro_desc_txtView);
+        CheckBox cb1 = (CheckBox) convertView.findViewById(R.id.checkBox1);
+        cb1.setVisibility(View.INVISIBLE);
 
         // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
         BistroListViewItem listViewItem = listViewItemList.get(position);
