11bbc317ec60cebcc7be4b5b4ccc51803917dee3
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2020-09-04 22:59:33 +0900

    feat: Add code to show and work general order form

--- app/src/main/java/com/example/yaneodoo/Customer/ShowCustomerOrderForm.java
+++ app/src/main/java/com/example/yaneodoo/Customer/ShowCustomerOrderForm.java
@@ -2,17 +2,28 @@ package com.example.yaneodoo.Customer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.yaneodoo.R;
 
 public class ShowCustomerOrderForm extends AppCompatActivity {
-    // FOR ACTIVITY SWITCH. SHOW USER'S ORDER FORM.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_form_customer);
+
+        Button pickupbtn = (Button) findViewById(R.id.btn_pick_up);
+        pickupbtn.setOnClickListener(new TextView.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowCustomerOrderForm.this.finish();
+                startActivity(new Intent(ShowCustomerOrderForm.this, ShowCustomerMenuList.class));
+            }
+        });
     }
 
     @Override
